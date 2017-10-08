ec5aec102554d39c08865b495276f26736e32f2c
Maykel S. Braz <maykelsb@yahoo.com.br>
2017-10-07 21:13:57 -0300

    Initial database migrations

---
+++ database/migrations/2017_10_07_134719_create_user_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTable extends Migration
+{
+    protected $tableName = 'users';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->tableName, function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('user', 45)
+                ->comment('Name  used by the user to login and app display');
+            $table->string('password', 32)
+                ->comment('User login password used when it is a local auth, otherwise, empty')
+                ->nullable();
+            $table->string('fullname', 100)
+                ->comment('User full name');
+            $table->timestamps();
+        });
+
+        DB:statement("ALTER TABLE '{$this->tableName}' comment 'Stores app users'");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists($this->tableName);
+    }
+}
+
---
+++ database/migrations/2017_10_07_134816_create_sheetitem_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSheetitemTable extends Migration
+{
+    protected $tableName = 'sheetitems';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->tableName, function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('sheetid')
+                ->comment('References sheets.id');
+            $table->integer('identifier')
+                ->comment('Represents a number which identifies that sheet item in a dice row');
+            $table->string('description', 255)
+                ->comment('Sheet item description');
+            $table->integer('subsheetid')
+                ->nullable()
+                ->comment('References a table which is used as subtable');
+            $table->timestamps();
+
+            $table->foreign('sheetid')
+                ->references('id')
+                ->on('sheets');
+
+            $table->foreign('subsheetid')
+                ->references('id')
+                ->on('sheets');
+        });
+
+        DB:statement("ALTER TABLE '{$this->tableName}' comment 'Stores data sheet item or subtable'");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists($this->tableName);
+    }
+}
