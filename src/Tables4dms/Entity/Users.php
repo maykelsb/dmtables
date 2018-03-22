879ec41d5da256f770f4a874e166168e9d57f9a7
Maykel S. Braz <maykelsb@yahoo.com.br>
2018-03-21 22:02:12 -0300

    Users entity

---
+++ src/Tables4dms/Entity/Users.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tables4dms\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="users")
+ */
+class Users
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $user;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $fullname;
+}
+
